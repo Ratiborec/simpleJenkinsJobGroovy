29030800ea205dacdcc06b508d98745d30a5ee98
Ratiborec <mikhailznak@gmail.com>
2018-07-29 11:10:04 +0300

    Update simpleJob.groovy

--- simpleJob.groovy
+++ simpleJob.groovy
@@ -9,15 +9,16 @@ import hudson.plugins.ws_cleanup.*
 import com.cloudbees.hudson.plugins.folder.*
 import org.jenkinsci.plugins.*
 
-
 Jenkins jenkins = Jenkins.instance
-def job = jenkins.createProject(FreeStyleProject.class, "name")
+def job
+job = jenkins.createProject(FreeStyleProject.class, "MNT-CD-module9-extcreated-job")
 job.getBuildWrappersList().add(new PreBuildCleanup( new ArrayList<Pattern>(), false,"","") );
-job.setScm(new GitSCM('git'));
+job.setScm(new GitSCM('https://github.com/Ratiborec/hello_world'));
+job.scm.branches[0].name = '*/${BRANCH_NAME}'
+job.addProperty(new ParametersDefinitionProperty(new StringParameterDefinition("BRANCH_NAME","master")))
 job.getBuildersList().add(new hudson.tasks.Shell('echo "Hello World"'));
 job.save()
 
 def matchedJobs = Jenkins.instance.items.findAll { check ->
     println check
 }
-
